671a9ded68392787975b77bbd8b2ac4d5145154b
Shahzaib Hassan <163294249+Shahzaibhassan123@users.noreply.github.com>
2024-06-01 16:58:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Graphic designer", "UI UX Designer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
